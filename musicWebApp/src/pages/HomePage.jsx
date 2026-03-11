120caa8884b703c11268834adbe2f8959ea5faab
Rairura <biryukova.ira63@gmail.com>
2026-03-11 22:22:04 +0300

    view all btm layout

--- musicWebApp/src/pages/HomePage.jsx
+++ musicWebApp/src/pages/HomePage.jsx
@@ -1,11 +1,22 @@
 import { HomeHeader } from "../assets/HomeHeader"
-import {JoinUsElem} from "../assets/JoinUsElem"
+import { JoinUsElem } from "../assets/JoinUsElem"
 
 export const HomePage = () => {
     return (<>
         <div className="homePageWrapper">
             <HomeHeader></HomeHeader>
-            <h2>Weekly Top <span className="pinkText">Songs</span></h2>
+
+            <div className="weeklyTopSection">
+                <h2>Weekly Top <span className="pinkText">Songs</span></h2>
+                <div className="viewAllBtnWrapper">
+                    <button className="viewAllBtn">+</button>
+                    <span>View All</span>
+                </div>
+
+
+            </div>
+
+
             <h2>New Release <span className="pinkText">Songs</span></h2>
             <h2>Trending <span className="pinkText">Songs</span></h2>
             <h2>Popular <span className="pinkText">Artists</span></h2>
